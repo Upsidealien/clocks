60907ec800c741550f37d00397082aa50ae9bfe6
Thomas Cartwright <TJCARTWRIGHT91@GMAIL.COM>
2016-11-21 22:00:55 +1100

    background colour updating and div centered

--- hex.js
+++ hex.js
@@ -1,3 +1,26 @@
 window.onload = function() {
-  document.getElementById("hex").innerHTML = "sup";
+
+  var d, h, m, s, colour;
+
+  var displayTime = function() {
+
+      d = new Date();
+      h = d.getHours();
+      m = d.getMinutes();
+      s = d.getSeconds();
+
+      if(h <= 9) { h = '0' + h.toString() }
+      if(m <= 9) { m = '0' + m.toString() }
+      if(s <= 9) { s = '0' + s.toString() }
+
+      color = "#" + h.toString() + m.toString() + s.toString();
+
+      document.body.style.backgroundColor = color;
+      document.getElementById("hex").innerHTML = color;
+
+      setTimeout(displayTime, 1000);
+  };
+
+  displayTime();
+
 }
