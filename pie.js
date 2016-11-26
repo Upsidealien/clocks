8d4919a6ce3e5188fbcf196f84dd5b0f74173970
Thomas Cartwright <TJCARTWRIGHT91@GMAIL.COM>
2016-11-26 18:31:58 +1100

    donut graph updates on screen

--- pie.js
+++ pie.js
@@ -1,34 +1,46 @@
-//var seconds = 45;
+var dataset = [40, 20];
 
-drawPie(30);
+var width = 460,
+height = 300,
+radius = Math.min(width, height) / 2;
 
-function drawPie(seconds) {
-  var dataset = {
-    apples: [seconds, 100-seconds],
-  };
+var color = ['#000000', '#FFFFFF'];
 
-  var width = 460,
-      height = 300,
-      radius = Math.min(width, height) / 2;
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var color = ['#000000', '#FFFFFF'];
+function render(data) {
+var pie = d3.pie()
+  .sort(null);
 
-  var pie = d3.pie()
-      .sort(null);
+var arc = d3.arc()
+  .innerRadius(radius - 100)
+  .outerRadius(radius - 50);
 
-  var arc = d3.arc()
-      .innerRadius(radius - 100)
-      .outerRadius(radius - 50);
+var path = svg.selectAll("path")
+  .data(pie(data));
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var pathEnter = path.enter().append("path")
+  .attr("fill", function(d, i) { return color[i]; })
+  .attr("d", arc);
 
-  var path = svg.selectAll("path")
-      .data(pie(dataset.apples))
-      .enter().append("path")
-      .attr("fill", function(d, i) { return color[i]; })
-      .attr("d", arc);
-};
+var pathUpdate = path.attr("d", arc);
+
+
+}
+
+render(dataset);
+
+setInterval(function(){
+update();
+}, 2000);
+
+function update() {
+
+dataset = [Math.random()*50, Math.random()*50];
+
+render(dataset);
+}
